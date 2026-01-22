29e10bd16e3e2d8253afca92ddd02e0ebecebf6a
luffy <luffybuahkaret@gmail.com>
2026-01-22 09:15:35 +0800

    Hello Rusty Crab!

---
+++ Hello Rusty Crab!/helloCrab/target/debug/deps/helloCrab.d
@@ -0,0 +1,5 @@
+D:\Project\RustLang-Learning\Hello Rusty Crab!\helloCrab\target\debug\deps\helloCrab.d: src\main.rs
+
+D:\Project\RustLang-Learning\Hello Rusty Crab!\helloCrab\target\debug\deps\helloCrab.exe: src\main.rs
+
+src\main.rs:
---
+++ Hello Rusty Crab!/helloCrab/target/debug/helloCrab.d
@@ -0,0 +1,1 @@
+D:\Project\RustLang-Learning\Hello\ Rusty\ Crab!\helloCrab\target\debug\helloCrab.exe: D:\Project\RustLang-Learning\Hello\ Rusty\ Crab!\helloCrab\src\main.rs
